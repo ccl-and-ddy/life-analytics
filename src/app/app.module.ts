f04945b2e72d4606957415e74e31dbd38b1da392
Duncan C <frigid.rain@gmail.com>
2021-06-13 17:02:17 -0700

    Fix build errors.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { NavComponent } from './core/components/nav/nav.component';
 import { HomeComponent } from './modules/pages/home/home.component';
-import { TemplateSelectorComponent } from './template-selector/template-selector.component';
 import { AboutComponent } from './modules/pages/about/about.component';
 import { DashboardTypeSelectorComponent } from './modules/components/dashboard-type-selector/dashboard-type-selector.component';
-import { DailyDashboardComponent } from './modules/components/daily-dashboard/daily-dashboard.component';
-import { DashboardDailyComponent } from './modules/components/dashboard-daily/dashboard-daily.component';
-import { DashboardAmountComponent } from './modules/components/dashboard-amount/dashboard-amount.component';
+import { DailyDashboardComponent } from './modules/components/dashboard-types/daily-dashboard/daily-dashboard.component';
 import { AmountDashboardComponent } from './modules/components/dashboard-types/amount-dashboard/amount-dashboard.component';
 
 @NgModule({
@@ -20,12 +17,9 @@ import { AmountDashboardComponent } from './modules/components/dashboard-types/a
     HeaderComponent,
     NavComponent,
     HomeComponent,
-    TemplateSelectorComponent,
     AboutComponent,
     DashboardTypeSelectorComponent,
     DailyDashboardComponent,
-    DashboardDailyComponent,
-    DashboardAmountComponent,
     AmountDashboardComponent
   ],
   imports: [
