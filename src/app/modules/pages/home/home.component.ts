d794abc2b401aacb350d14fee319e5c17b325c96
Duncan C <frigid.rain@gmail.com>
2021-06-13 18:41:32 -0700

    Close overlay on outside click.

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,17 +1,27 @@
+import { ElementRef } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
 })
 export class HomeComponent implements OnInit {
 
   isSelectorOpen = false;
 
-  constructor() { }
+  constructor(private _eref: ElementRef) { }
 
   ngOnInit(): void {
+  }
+
+  onClick(event: Event) {
+    if (!this._eref.nativeElement.contains(event.target)) {
+      this.isSelectorOpen = false;
+    }
   }
 
   openSelector() {
